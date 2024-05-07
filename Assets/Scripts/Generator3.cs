f361fe942fa05965392177ee609ab83b1bad714f
Lyshaka <maxence.daboville@outlook.fr>
2024-05-07 08:56:48 +0200

    Created a script that i probably won't use :|

---
+++ Assets/Scripts/Generator3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator3 : MonoBehaviour
+{
+	private enum Direction
+	{
+		up = 1,
+		down = -1,
+		left = 2,
+		right = 3,
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
